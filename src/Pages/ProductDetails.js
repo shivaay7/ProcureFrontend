e130fac98baf64dd75c53210069a5a138ff2691e
Shivam Mishra <shivammishra7613@gmail.com>
2023-08-25 23:40:29 +0530

    Update ProductDetails.js

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -30,7 +30,7 @@ const ProductDetails = () => {
   const getSimilarProduct = async (pid, cid) => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/related-product/${pid}/${cid}`
+        `https://procurebackend.onrender.com/api/v1/product/related-product/${pid}/${cid}`
       );
       setRelatedProducts(data?.products);
     } catch (error) {
@@ -42,7 +42,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <img
-            src={`/api/v1/product/product-photo/${product._id}`}
+            src={`https://procurebackend.onrender.com/api/v1/product/product-photo/${product._id}`}
             className="rounded mx-auto d-block pt-5 mt-5"
             alt={product.name}
             height="300"
@@ -75,7 +75,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`https://procurebackend.onrender.com/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
